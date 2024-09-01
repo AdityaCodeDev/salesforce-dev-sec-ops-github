cfc6e80db0d249141d34a1d82aba04f1bef1909c
AdityaCodeDev <adityakaveri98@gmail.com>
2024-09-02 00:47:07 +0530

    Added the Asset Trigger

--- force-app/main/default/lwc/contactTile/contactTile.js
+++ force-app/main/default/lwc/contactTile/contactTile.js
@@ -1,3 +1,8 @@
 import { LightningElement } from 'lwc';
 
-export default class ContactTile extends LightningElement {}
+export default class ContactTile extends LightningElement {
+    /*** Create a method to Calculate the sum of 3 numberd: */
+    calculationSum(num1,num2){
+        return num1 + num2;
+    }
+}
